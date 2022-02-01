767fe7f30e4d210ab61f1730a7b2356e92c4bb4e
Fabio Riccardi <fabio@glass-imaging.com>
2022-02-01 10:06:47 -0800

    Empty project commit

---
+++ LightTable/LightTable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LightTable
+//
+//  Created by Fabio Riccardi on 2/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LightTable/LightTable/LightTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightTableApp.swift
+//  LightTable
+//
+//  Created by Fabio Riccardi on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LightTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
