36325946336fd031450ab426f88ce564b8ae4f64
Fabio Riccardi <fabio@glass-imaging.com>
2022-02-10 14:56:52 -0800

    Found a fix for the folder tree double click issue.

--- LightTable/LightTable/Views/FolderTreeDisclosure.swift
+++ LightTable/LightTable/Views/FolderTreeDisclosure.swift
@@ -17,23 +17,23 @@ struct FolderTreeDisclosure: View {
         var hasImages = false
         let children = folderListingAt(url: url, hasImages: &hasImages)
 
-        if (children.isEmpty) {
+        if children.isEmpty {
             Label(url.lastPathComponent, systemImage: hasImages ? "folder.fill" : "folder")
         } else {
             DisclosureGroup(isExpanded: $expanded, content: {
-                if (expanded) {
+                if expanded {
                     ForEach(children, id: \.self) { item in
                         FolderTreeDisclosure(url: item, selection: _selection, doubleTapAction: doubleTapAction)
                     }
                 }
             }, label: {
-                VStack {
-                    Label(url.lastPathComponent, systemImage: hasImages ? "plus.rectangle.on.folder.fill" : "folder.fill")
-                }
-                // Breaks the List single click selection
-                // .onTapGesture(count: 2) {
-                //     doubleTapAction(url)
-                // }
+                Label(url.lastPathComponent, systemImage: hasImages ? "plus.rectangle.on.folder.fill" : "folder.fill")
+                    .onTapGesture(count: 2, perform: {
+                        doubleTapAction(url)
+                    })
+                    .onTapGesture(count: 1, perform: {
+                        selection = [url]
+                    })
             })
         }
     }
