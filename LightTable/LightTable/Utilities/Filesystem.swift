0037c7e60bb2038b2e8580fae19cebf54c0f8a8c
Fabio Riccardi <fabio@glass-imaging.com>
2022-02-28 13:01:40 -0800

    Added copyright statement to source files, removed some unused code.

--- LightTable/LightTable/Utilities/Filesystem.swift
+++ LightTable/LightTable/Utilities/Filesystem.swift
@@ -1,9 +1,17 @@
+// Copyright (c) 2022 Glass Imaging Inc.
+// Author: Fabio Riccardi <fabio@glass-imaging.com>
 //
-//  Filesystem.swift
-//  LightTable
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Fabio Riccardi on 2/3/22.
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import SwiftUI
 
@@ -20,8 +28,9 @@ func NSOpenPanelDirectoryListing(files:inout[URL]) -> URL? {
     return nil
 }
 
+private let imageFileExtensions = ["jpg", "jpeg", "png", "heic", "heif", "dng", "cr3", "arw", "crw", "cr2"]
+
 func isImage(file:URL) -> Bool {
-    let imageFileExtensions = ["jpg", "jpeg", "png", "heic", "heif", "dng", "cr3", "arw", "crw", "cr2"]
     let fileExtension = file.pathExtension.lowercased()
     return imageFileExtensions.contains(fileExtension)
 }
--- LightTable/LightTable/Views/ImageBrowserView.swift
+++ LightTable/LightTable/Views/ImageBrowserView.swift
@@ -1,18 +1,23 @@
+// Copyright (c) 2022 Glass Imaging Inc.
+// Author: Fabio Riccardi <fabio@glass-imaging.com>
 //
-//  ImageBrowserView.swift
-//  LightTable
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Fabio Riccardi on 1/31/22.
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import SwiftUI
 
 struct ImageBrowserView: View {
     @Binding var browserModel:ImageBrowserModel
     @Binding var viewModel:ImageViewModel
-
-    // Height of the ThumbnailScrollerPanel, modified by the PaneDivider
-    @State var scrollViewHeight:CGFloat = 200
 
     let backgroundColor = Color(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0)
     let dividerColor = Color(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0)
